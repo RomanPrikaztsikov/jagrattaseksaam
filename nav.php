32984e300a2ae3f8237ad5c5de6638fc1905108b
Roman Prikaztsikov <mikolkyle@gmail.com>
2026-01-12 14:13:10 +0200

    nav ja style failid lisatud

---
+++ nav.php
@@ -0,0 +1,11 @@
+<nav>
+    <ul>
+        <li><a href="registreerimine.php">Registreerimine</a></li>
+        <li><a href="teooriaeksam.php">Teooriaeksam</a></li>
+        <li><a href="slaalom.php">Slaalom</a></li>
+        <li><a href="ringtee.php">Ringtee</a></li>
+        <li><a href="tanav.php">Tänavasõit</a></li>
+        <li><a href="lubadeleht.php">Lõpetamine</a></li>
+    </ul>
+</nav>
+<link rel="stylesheet" type="text/css" href="style.css">
