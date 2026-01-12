bb5393f44bc88b00cb4c8f17f602f29ad4ac21b0
Roman Prikaztsikov <mikolkyle@gmail.com>
2026-01-12 13:07:55 +0200

    config ja registreerimine loodud

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+$baasiaadress="localhost";
+$baasikasutaja="juku";
+$baasiparool="1234";
+$baasinimi="juku";
+$yhendus=new mysqli($baasiaadress, $baasikasutaja, $baasiparool, $baasinimi);  //PHP lõpumärki pole vaja, et kogemata midagi välja ei trükitaks
